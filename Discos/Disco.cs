79decd4e41b31f3ea48701294ca0464b1af839c6
Diego Di Stefano <diego.distefano.1989@gmail.com>
2023-05-22 17:35:37 -0300

    Discos y Pokedex

---
+++ Discos/Disco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discos
+{
+    class Disco
+    {
+        public String Titulo { get; set; }
+        public int CantidadCanciones { get; set; }
+        public String FechaLanzamiento { get; set; }
+    }
+}
